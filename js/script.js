12b62ee810d2a8f44d3fc678d9ed849511bccbf9
kotamobagudev <blackz.wd@gmail.com>
2021-02-13 05:06:11 +0700

    js 66%

--- js/script.js
+++ js/script.js
@@ -14,10 +14,34 @@ function animasiButton(){
                 });
 }
 
-function animasiIntroOut(){
-    $('#start').velocity("transition.whirlOut", {stagger: 150, complete: function(){
-        $('#text').velocity({"font-size":"20px", "top":"95%"}, {duration: 1000});
-    }});
+function animasitIntroOut(){
+    $("#start").attr('disabled', true).css({"color":"black"});
+    $("#start").velocity("transition.whirlOut",{
+                                stagger: 150,
+                                complete: function(){
+                                    $("#text").velocity({"font-size":"20px", 
+                                                         "top":"95%"
+                                                        }, {
+                                                            duration: 1000,
+                                                            complete: function(){
+                                                                callMenu();
+                                                                $("#menu ul li a[href='what_we_do']").trigger("click");
+                                                                $("#start").attr('disabled', false);
+                                                            }
+                                                        });
+                                }
+                            });
+}
+
+function callMenu(){
+    $("#menu ul li").velocity("transition.slideLeftIn",{
+                                stagger: 250
+                             });
+
+    $("#menu ul li a").off("click").click(function(event){
+        event.preventDefault();
+        $(this).parent("li").addClass("active").siblings().removeClass("active");
+    });
 }
 
 $(document).ready(function(){
