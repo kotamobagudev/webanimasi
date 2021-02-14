212d83c6f2a7bcf7dd06c446320dc420fbd1a217
IndraSatya94 <78789842+IndraSatya94@users.noreply.github.com>
2021-02-14 16:11:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function animasiButton(){
 }
 
 function animasitIntroOut(){
-    $("#start").attr('disabled', true).css({"color":"black"});
+    $("#start").attr("disabled", true).css({"color":"black"});
     $("#start").velocity("transition.whirlOut",{
                                 stagger: 150,
                                 complete: function(){
@@ -26,6 +26,7 @@ function animasitIntroOut(){
                                                             complete: function(){
                                                                 callMenu();
                                                                 $("#menu ul li a[href='what_we_do']").trigger("click");
+                                                                $("#start").attr("disabled", false).css({"color":"black"});
                                                             }
                                                         });
                                 }
@@ -37,14 +38,24 @@ function callMenu(){
                                 stagger: 250
                              });
 
-    $("#menu ul li a").click(function(event){
+    $("#menu ul li a").off().click(function(event){
         event.preventDefault();
         $(this).parent("li").addClass("active").siblings().removeClass("active");
 
         var hrefString = $(this).attr("href");
-        $("#" + hrefString).show();
-        window[hrefString]();
-    });
+        if(hrefString == "back_to_intro"){
+            back_to_intro();
+        } else {
+
+            if (!$("#" + hrefString).is(":visible")){
+                $(".container-content").fadeOut(1000);
+                setTimeout(function(){           
+                    $("#" + hrefString).show();
+                    window[hrefString]();
+                }, 1000);
+            }
+        }
+     });
 }
 
 function what_we_do(){
@@ -52,6 +63,26 @@ function what_we_do(){
     $("what_we_do .title").velocity("transition.slideUpIn", {duration:1500});
     $("what_we_do div").velocity("transition.slideDownIn", {duration:1500});
 }
+
+
+function our_team(){
+    $(".members.top240").velocity("transition.slideUpIn",{duration:100});
+    $(".members.top170").velocity("transition.slideUpIn",{duration:100});
+}
+
+
+function back_to_intro(){
+    $("#menu ul li").hide();
+    $("#.container-content").hide();
+    $("#text").velocity({"font-size":"90px", 
+                            "top":"50%"
+                        }, {
+                            duration: 1000,
+                            complete: function(){
+                                $("#start").velocity("transition.whirlIn");
+                            }
+                        });
+
 
 $(document).ready(function(){
     animasiInfo();
